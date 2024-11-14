2047d8093b7f400fa512f592c6b768810203ca43
ShriganeshBhandari09 <shriganesh.bhandari693@gmail.com>
2024-11-14 22:10:41 +0530

    Strong Password Solution

---
+++ strongPasssword.js
@@ -0,0 +1,44 @@
+//Question link: https://www.hackerrank.com/challenges/strong-password/problem?isFullScreen=true
+
+// Louise joined a social networking site to stay in touch with her friends. The signup page required her to input a name and a password. However, the password must be strong. The website considers a password to be strong if it satisfies the following criteria:
+
+// Its length is at least .
+// It contains at least one digit.
+// It contains at least one lowercase English character.
+// It contains at least one uppercase English character.
+// It contains at least one special character. The special characters are: !@#$%^&*()-+
+// She typed a random string of length  in the password field but wasn't sure if it was strong. Given the string she typed, can you find the minimum number of characters she must add to make her password strong?
+
+// Input 3,Ab1
+// Length is 3
+// Password is Ab1
+// Output 3
+
+function strongPassword(n, password) {
+  let count = 0;
+  if (!password.match(/[ABCDEFGHIJKLMNOPQRSTUVWXYZ]/)) {
+    count++;
+    console.log("Password should includes capital letters");
+  }
+  if (!password.match(/[0123456789]/)) {
+    count++;
+    console.log("Password should includes numbers");
+  }
+  if (!password.match(/[abcdefghijklmnopqrstuvwxyz]/)) {
+    count++;
+    console.log("Password length should includes small letters");
+  }
+  if (!password.match(/[!@#$%^&*()-+]/)) {
+    count++;
+    console.log("Password should include special characters");
+  }
+  if (n <= 6) {
+    count++;
+    console.log("Password length should be 6");
+  }
+  return count
+}
+
+console.log(strongPassword(11, "#HackerRank"));
+
+//TODO: Not full Solution , I need to change some code
